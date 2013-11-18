503f8e61a25731f6eb455bf07339df376b5b4ba0
Rohan Barman <Rohan1Barman@gmail.com>
2013-11-18 16:10:24 -0500

    Added action bar with clickable homebutton + search widget.Sample Background included aswell.

--- src/com/example/brainyquote/MainActivity.java
+++ src/com/example/brainyquote/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.brainyquote;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //creates an action bar. 
+        ActionBar actionBar = getActionBar();
+        //sets the homebutton. 
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
 
@@ -17,6 +26,11 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
     
