f8e088cd7a98746ea306767bb0964eba255125b5
Rohan Barman <Rohan1Barman@gmail.com>
2013-11-20 19:50:46 -0500

    Multi threading on random quote page (async task)

--- src/com/example/brainyquote/MainActivity.java
+++ src/com/example/brainyquote/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SearchView;
-import android.widget.SearchView.OnQueryTextListener;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
--- src/com/example/brainyquote/RandomQuote.java
+++ src/com/example/brainyquote/RandomQuote.java
@@ -1,19 +1,63 @@
 package com.example.brainyquote;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class RandomQuote extends Activity {
+	
 
+	//This is the activity launched when the user selects the randomButton on main activity.
+	Handler handler;
+	TextView textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_random_quote);
 		// Show the Up button in the action bar.
 		setupActionBar();
+	
+		handler = new Handler();
+		textView = (TextView)findViewById(R.id.textView);
+		//execute the async task
+		new GetTitle().execute();
+		
+	}
+	//need to create an AsyncTask so that the UI thread does not have to do extra work
+	//if we make the UI thread to the Jsoup.connect, the application will crash.
+	//currently I am just pulling the title from Google as a simple test. 
+	private class GetTitle extends AsyncTask<Void, Void, String> {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			 Document doc;
+	         try {
+	                doc = Jsoup.connect("http://www.google.com").userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").get();
+	                return doc.title();
+	         } catch (IOException e) {
+	                return null;
+	            }
+			}
+		@Override
+		protected void onPostExecute(String title){
+			TextView textView = (TextView)findViewById(R.id.textView);
+			textView.setText(title);
+		}
+	
 	}
 
 	/**
