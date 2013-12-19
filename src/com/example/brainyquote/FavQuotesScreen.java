042f96298f7dd879f96d8817d55d89eebaf17044
Rohan Barman <Rohan1Barman@gmail.com>
2013-12-18 20:05:18 -0600

    fav launcher will not be functional in FavQuoteScreen Activity

--- src/com/example/brainyquote/FavQuotesScreen.java
+++ src/com/example/brainyquote/FavQuotesScreen.java
@@ -167,10 +167,6 @@ public class FavQuotesScreen extends Activity {
 			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
-		case R.id.launch_fav_activity:
-    		Intent intent = new Intent(getBaseContext(), FavQuotesScreen.class);
-			startActivity(intent);
-			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
