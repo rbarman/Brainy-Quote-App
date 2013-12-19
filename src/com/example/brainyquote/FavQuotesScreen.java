8f8fceeb24a7b447bef4d828814c7cc836e39972
Rohan Barman <Rohan1Barman@gmail.com>
2013-12-18 18:05:08 -0600

    added search into Fav Activity's actionbar

--- src/com/example/brainyquote/FavQuotesScreen.java
+++ src/com/example/brainyquote/FavQuotesScreen.java
@@ -9,10 +9,16 @@ import java.io.IOException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class FavQuotesScreen extends Activity {
@@ -97,8 +103,51 @@ public class FavQuotesScreen extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.fav_quotes_screen, menu);
+			getMenuInflater().inflate(R.menu.fav_quotes_screen, menu);
+		// Inflate the menu; this adds items to the action bar if it is present.
+		        getMenuInflater().inflate(R.menu.main, menu);
+		        
+		        // Associate searchable configuration with the SearchView
+		        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+		        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+		        
+		        //setting hint value via .java.... should be able to do in .XML....
+		        searchView.setQueryHint("Search Brainy Quote!");
+		        
+		        final SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+		        	
+		    	    @Override
+		    	    public boolean onQueryTextChange(String newText) {
+		    	    	
+		    	        // Do something
+		    	        return true;
+		    	    }
+
+		    	    @Override
+		    	    public boolean onQueryTextSubmit(String query) {    	    	
+		   
+		    	        Toast toast = Toast.makeText(getApplicationContext(), query + " Quotes coming soon...", Toast.LENGTH_SHORT);
+		    	        toast.setGravity(Gravity.CENTER, 0, 0);
+		    	        toast.show();
+		    	        
+		    	      //Now a new intent will be created to go to the SpecificQuote.java activity! 
+						Intent intent = new Intent(getBaseContext(), SpecificQuote.class);
+						
+						//we will pass the value of query as a string variable called queryText to the SpecificQuote activity
+						//so the SpecificQuote activity can use the queryText as the search parameter. 
+						intent.putExtra("queryText", query);
+						startActivity(intent);	
+		    	        
+		    	        return true;
+		    	    }
+		    	};
+		    	searchView.setOnQueryTextListener(queryTextListener);
+		
+		
 		return true;
 	}
 
