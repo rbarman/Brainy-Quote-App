ff668ccfd78b36d8adadb09ab6ab9a6b0842e44b
Rohan Barman <Rohan1Barman@gmail.com>
2013-12-30 19:50:08 -0500

    added sharing to SelectedQuote Activity

--- src/com/example/brainyquote/SelectedQuote.java
+++ src/com/example/brainyquote/SelectedQuote.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class SelectedQuote extends BaseActivity {
 	ImageButton star;
+	ImageButton share;
 	boolean isFavorite = true;
 	
 	@Override
@@ -45,6 +46,21 @@ public class SelectedQuote extends BaseActivity {
 					Toast.makeText(getApplicationContext(), "Quote is added to favorites!", Toast.LENGTH_SHORT).show();
 					isFavorite = true;
 				}
+			}
+		});
+		
+		share = (ImageButton) findViewById(R.id.share);
+		share.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "clicked",
+						Toast.LENGTH_SHORT).show();
+				Intent shareIntent = new Intent();
+				shareIntent.setAction(Intent.ACTION_SEND);
+				shareIntent.putExtra(Intent.EXTRA_TEXT, sharingQuote);
+				shareIntent.setType("text/plain");
+				startActivity(Intent.createChooser(shareIntent, "Share via"));
 			}
 		});
 		
